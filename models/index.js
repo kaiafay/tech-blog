fd42e6e5457edc766c2a84a71b97bc3d91fc8446
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-04 17:49:54 -0700

    create associations

---
+++ models/index.js
@@ -0,0 +1,22 @@
+// import models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// define model associations
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'postId',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
