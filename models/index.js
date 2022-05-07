cc549f95a11936f3a028f3a66115d573177470b4
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-07 12:50:27 -0700

    add additional association between user and post

--- models/index.js
+++ models/index.js
@@ -9,6 +9,11 @@ Post.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
+User.hasMany(Post, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
 Post.hasMany(Comment, {
     foreignKey: 'postId',
     onDelete: 'CASCADE'
