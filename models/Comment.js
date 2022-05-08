90bd1aad56630a5b3885f6a0daa29e9391ca3c7a
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-08 16:39:52 -0700

    add references to user and post in Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -5,9 +5,35 @@ class Comment extends Model { }
 
 Comment.init(
     {
-        body: {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                // comment cannot be empty
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
         }
     }, 
     {
