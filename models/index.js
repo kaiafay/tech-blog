d95755eecbf0a19474853b7cd7b31dfeaae7f6f2
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-08 16:50:18 -0700

    fix associations according to models

--- models/index.js
+++ models/index.js
@@ -9,18 +9,25 @@ Post.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
-User.hasMany(Post, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
-});
-
 Post.hasMany(Comment, {
     foreignKey: 'postId',
-    onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'userId'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'userId'
 });
 
 Comment.belongsTo(User, {
     foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'postId',
     onDelete: 'CASCADE'
 });
 
