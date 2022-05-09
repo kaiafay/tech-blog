88d1daae7e0057ed1c96023d575a4491b183816a
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-08 18:04:04 -0700

    render a single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
         }
       ]
     })
-    .then((postData) => {
+    .then(postData => {
       const posts = postData.map(post => post.get({ plain: true }));
       res.render('homepage', { posts, loggedIn: req.session.loggedIn})
     })
@@ -29,5 +29,39 @@ router.get('/', (req, res) => {
     })
 });
 
+// render a single post
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'title', 'body', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }, 
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(postData => {
+    if(!postData) {
+      res.status(404).json({ message: 'No post found with this ID!' });
+      return;
+    }
+    const post = postData.get({ plain: true });
+    res.render('single-post', { post, loggedIn: req.session.loggedIn });
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
