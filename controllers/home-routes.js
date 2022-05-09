5fe7c376ef41098161de4db49e0e1bb43eb2eada
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-08 17:32:04 -0700

    include user and date info when displaying posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,33 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 // render homepage
 router.get('/', (req, res) => {
     Post.findAll({
-        include: [User],
+      attributes: ['id', 'title', 'body', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
-    .then(postData => {
-        const posts = postData.map(post => post.get({ plain: true}));
-        res.render('homepage', { posts })
+    .then((postData) => {
+      const posts = postData.map(post => post.get({ plain: true }));
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn})
     })
     .catch(err => {
-        res.status(500).json(err);
-    }) 
+      res.status(500).json(err);
+    })
 });
+
 
 module.exports = router;
