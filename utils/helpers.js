cc12da1e4ff76b22a16aabdfe84dccf647b93c8a
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-15 12:55:17 -0700

    write helper function for date formatting

---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // format date helper function
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    }
+};
