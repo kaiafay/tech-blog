fa23a15b21a266596e42fbd80a672394a597fbd2
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-15 14:02:33 -0700

    add login functionality

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -24,5 +24,31 @@ async function signupFormHandler(e) {
     };
 };
 
+// function for login
+async function loginFormHandler(e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username-login').value.trim();
+    const password = document.getElementById('password-login').value.trim();
+
+    if(username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if(response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    };
+};
+
 // event listener for signup button
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
