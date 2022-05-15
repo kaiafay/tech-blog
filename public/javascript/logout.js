aad93af5a94bd1c782bc5a8ebd37e78458587891
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-15 14:02:46 -0700

    add logout functionality

---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+// function for logout
+async function logout() {
+    const response = await fetch('api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if(response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+// event listener for logout button
+document.querySelector('#logout').addEventListener('click', logout);
