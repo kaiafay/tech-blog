eed6cc96450eb4578469171a6c8231126ff0c49a
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-17 20:18:41 -0700

    create all post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
 router.use('/comment', commentRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// create a post
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        user_id: req.session.user_id
+    })
+    .then(postData => {
+        res.json(postData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// update a post
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        body: req.body.body
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    }
+  )
+  .then(postData => {
+      if(!postData) {
+          res.status(404).json({ message: 'No post found with this ID.' });
+          return;
+      }
+      res.json(postData);
+  })
+  .catch(err => {
+      res.status(500).json(err);
+  });
+});
+
+// delete a post
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        },
+    })
+    .then(postData => {
+        if(!postData) {
+            res.status(404).json({ message: 'No post found with this ID.' });
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
