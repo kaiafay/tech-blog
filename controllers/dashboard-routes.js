c74c59eead6defd389928fc4e50ebeb07e4fd123
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-19 17:07:06 -0700

    create edit post route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -35,4 +35,37 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+// edit post route
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: ['id', 'title', 'body', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        if(postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('edit-post', { post, loggedIn: true })
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
