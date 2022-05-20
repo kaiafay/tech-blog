deae24738eeff249474e9139cabfe91481e7ab96
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-19 17:18:39 -0700

    add edit post functionality

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,29 @@
+// global variable for id
+const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+];
+
+// function for editing post 
+async function editPostHandler(e) {
+    e.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'put',
+        body: JSON.stringify({
+            title,
+            body
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if(response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPostHandler);
